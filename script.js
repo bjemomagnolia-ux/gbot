662cadfbdd294cb294cb93ed1a416b1dae6fa0f2
Gerardo De La Cruz <b.jemomagnolia@gmail.com>
2026-06-18 23:32:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Función para mostrar la descripción técnica del proyecto
+function mostrarDetalle(nombreProyecto, descripcion) {
+    alert("🤖 PROYECTO: " + nombreProyecto + "\n\n⚙️ Funcionamiento: " + descripcion);
+}
